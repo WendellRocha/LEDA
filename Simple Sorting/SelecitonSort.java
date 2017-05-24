54f6e08b7822b71ec4a5c76db41e77130c44564b
Wendell Rocha <wendellrocha@outlook.com>
2017-05-24 21:44:47 +0000

    SelectionSort

---
+++ Simple Sorting/SelecitonSort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class SelectionSort {
+
+    public static void selectionSort(int [] array, int leftIndex, int rightIndex) {
+        int menor;
+        int indice;
+        
+        for(int i = leftIndex; i < rightIndex; i++) {
+            menor = array[i];
+            indice = i;
+            for(int j = i + 1; j <= rightIndex; j++) {
+                if(array[j].compareTo(menor) < 0) {
+                    menor = array[j];
+                    indice = j;
+                }
+            }
+            array[indice] = array[i];
+            array[i] = menor;
+        }
+    }
+
+    public static void selectionSort(int [] array) {
+        int menor;
+        int indice;
+        for(int i = 0; i < array.length; i++) {
+            menor = array[i];
+            indice = i;
+            for(int j = i + 1; j <= array.length; j++) {
+                if(array[j].compareTo(menor) < 0) {
+                    menor = array[j];
+                    indice = j;
+                }
+            }
+            array[indice] = array[i];
+            array[i] = menor;
+        }
+    }
+}

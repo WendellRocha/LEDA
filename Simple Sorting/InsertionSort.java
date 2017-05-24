aa680d998f69d7f0f00accfafea70c2484548161
Wendell Rocha <wendellrocha@outlook.com>
2017-05-24 20:44:11 +0000

    InsertionSort

---
+++ Simple Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class InsertionSort {
+    public static void insertionSort(int [] array, int leftIndex, int rightIndex) {
+        for(int i = leftIndex; i <= rightIndex; i++){
+            int key = array[i];
+            int j = i - 1;
+            while(j >= leftIndex && array[j].compareTo(key) > 0) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+    }
+
+    public static void insertionSort(int [] array) {
+        for(int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while((j > -1) && array[j].compareTo(key) > 0) {
+                array[j+1] = array [j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+    }
+}

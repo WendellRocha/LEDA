5195231e99b2021834ba37ac1ba69e96650faa9d
Wendell Rocha <wendellrocha@outlook.com>
2017-05-24 22:13:17 +0000

    RecurssiveInsertionSort

---
+++ Simple Sorting/RecursiveInsertionSort.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class RecursiveInsertionSort {
+    public static void recursiveInsertionSort(int [] array, int leftIndex, int rightIndex) {
+        if(leftIndex < rightIndex) {
+            for(int j = leftIndex; j < rightIndex; j++) {
+                if(array[j+1].compareTo(array[j]) < 0) {
+                    swap(array, j, j+1);
+                }
+            }
+            for(int i = rightIndex - 1; i > leftIndex; i--) {
+                if(array[i-1].compareTo(array[i]) > 0) {
+                    swap(array, i, i-1);
+                }
+            }
+
+            recursiveInsertionSort(array, leftIndex + 1, rightIndex - 1);
+        }
+    }
+}

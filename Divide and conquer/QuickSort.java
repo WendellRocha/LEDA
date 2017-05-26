2531409859c032f1d0ff9110ec18d98b1ac3d6a8
Wendell Rocha <wendellrocha@outlook.com>
2017-05-26 20:50:09 +0000

    QuickSort

---
+++ Divide and conquer/QuickSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class QuickSort {
+
+    public static void quickSort(int [] array, int leftIndex, int rightIndex) {
+        if(array == null || leftIndex < 0 || leftIndex > rightIndex || rightIndex > array.length)
+            throw new IllegalArgumentException();
+
+        if(leftIndex < rightIndex) {
+            int pivot = partition(array, leftIndex, rightIndex);
+            quickSort(array, leftIndex, pivot - 1);
+            quickSort(array, pivot + 1, leftIndex);
+        }
+        
+    }
+
+    private int partition(int [] array, int begin, int end) {
+        int pivot = array[begin];
+        int i = begin;
+        for(int j = i + 1; j <= end; j++) {
+            if(array[j].compareTo(pivot) <= 0) {
+                swap(array, i, j);
+                i++;
+            }
+        }
+        swap(array, begin, i); 
+    }
+
+    private void swap(int [] array, int i, int j) {
+        if(array == null || i < 0 || j < 0)
+            throw new IllegalArgumentException();   
+        
+        int aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+
+    }
+}

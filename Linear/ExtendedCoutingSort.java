f9bbdaad473379cadc80aa0c7b516055c2b692a1
Wendell Rocha <wendellrocha@outlook.com>
2017-05-31 18:09:32 +0000

    Extended Couting Sort

---
+++ Linear/ExtendedCoutingSort.java
@@ -0,0 +1,66 @@
+/**
+ * Data Structures and Algorithms Laboratory
+ * Período 2017.1
+ * @author Wendell Rocha
+ */
+class ExtendedCoutingSort {
+
+    public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		if(array == null || rightIndex > array.length)
+			throw new IllegalArgumentException();
+		
+		int max = 0;
+		int min = 0;
+		for(int i = leftIndex; i <= rightIndex; i++) {
+			if(array[i].compareTo(max) > 0) {
+				max = array[i];
+			}
+			
+			if(array[i].compareTo(min) > 0) {
+				min = array[i];
+			}
+		}
+		max = Math.abs(max);
+		min = Math.abs(min);
+		countingSort(array, min, max, leftIndex, rightIndex);
+	}
+
+	private void countingSort(Integer[] array, int min, int max, int leftIndex, int rightIndex) {
+		int [] aux = new int[min + max + 1];
+		/**
+         * cria o array auxiliar e preenche-o com zeros
+         */
+        for(int i = leftIndex; i <= max; i++) {
+            aux[i] = 0;
+        }
+
+        /**
+         * realiza a contagem de ocorrencias no array desordenado
+         */
+        for(int i = leftIndex; i < rightIndex + 1; i++) {
+            aux[array[i] + min]++;
+        }
+
+        /**
+         * faz o complemento do número anterior
+         */
+        for(int i = 1; i <= min + max; i++) {
+            aux[i] += aux[i-1];
+        }
+
+        /**
+         * ordena o array da esquerda para a direita
+         */
+        int [] arrayOrdenado = new int [array.length];
+        for(int i = rightIndex; i >= leftIndex; i--) {
+        	arrayOrdenado[--aux[array[i] + min]] = array[i];
+        }
+
+        /**
+         * retorna os valores ordenados para o array de entrada
+         */
+        for(int i = leftIndex; i < rightIndex + 1; i++) {
+            array[i] = arrayOrdenado[i];
+        }
+    }
+}
